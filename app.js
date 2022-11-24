aa347366ee610aadf6d2f40e0f2542a683e980ea
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2022-11-24 10:24:06 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+    $routeProvider
+    .when('/main', {
+        templateUrl: 'main.html',
+        controller: 'mainController'
+    })  
+    .when('/second', {
+        templateUrl: 'second.html',
+        controller: 'secondController'
+    })
+});
+
+myApp.controller('mainController', ['$scope', function($scope) {
+  $scope.name = "world"
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+
+
+}]);
